e301e3f631d347133f3a6acb4048fad6bbabd081
Stefan Baur <kontakt@baur-itcs.de>
2017-11-28 22:50:59 +0100

    adding some sane firefox-esr defaults

---
+++ config/includes.chroot/etc/firefox-esr/firefox-esr.js
@@ -0,0 +1,4 @@
+//set some sane browser defaults
+lockPref("browser.rights.3.shown", true);
+lockPref("browser.startup.homepage_override.mstone","ignore");
+lockPref("browser.startup.homepage", "https://www.x2go.org");
