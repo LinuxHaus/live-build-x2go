a7804e1c302ece5c8299bc57a69bae473292c9d0
Stefan Baur <kontakt@baur-itcs.de>
2017-11-30 10:21:44 +0100

    removed firefox-esr.js, as it gets overwritten by package install. Needs to be patched at runtime instead.

--- config/includes.chroot/etc/firefox-esr/firefox-esr.js
+++
@@ -1,4 +0,0 @@
-//set some sane browser defaults
-lockPref("browser.rights.3.shown", true);
-lockPref("browser.startup.homepage_override.mstone","ignore");
-lockPref("browser.startup.homepage", "https://www.x2go.org");
